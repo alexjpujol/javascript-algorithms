86e72449be41cd8513c05dbedaa7af0242c7dd7f
alexjpujol <apujol@spotify.com>
2018-06-28 14:05:48 -0400

    updated merge sort

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -2,15 +2,17 @@
 //divide your unsorted list until you have a series of 1 item lists. Then you put them back together in order
 
 const mergeSort = list => {
-
+    //define our base case
     if (list.length === 1) {
         return list;
     }
 
     let mid = Math.floor(list.length / 2);
+    //split the array in two, make the recursive calls
     let left = list.slice(0, mid);
     let right = list.slice(mid);
 
+    //merge them back together
     return merge(mergeSort(left), mergeSort(right));
 }
 
@@ -19,17 +21,20 @@ const merge = (left, right) => {
     let leftIndex = 0;
     let rightIndex = 0;
     
+    //as long as the arrays have something in them, check elements from each array and sort them
     while (leftIndex < left.length && rightIndex < right.length) {
         if (left[leftIndex] < right[rightIndex]) {
             result.push(left[leftIndex]);
             left.slice(leftIndex, 1);
             leftIndex++;
-        } else if (left[leftIndex] >= right[rightIndex]) {
+        } else {
             result.push(right[rightIndex]);
             right.slice(rightIndex, 1);
             rightIndex++;
         }
     }
+
+    //if one of the arrays runs out before the other, then attach the remainder of the longer array to results
     return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
